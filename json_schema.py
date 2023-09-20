5cdb58ab481b7e39f17ccd5615d6d0eb4452faa6
caiolul <caiolucena.il@gmail.com>
2023-09-20 09:46:13 -0300

    feat: Improved ifs in parse function

--- json_schema.py
+++ json_schema.py
@@ -4,6 +4,14 @@ from jsonschema import Draft7Validator, validators
 
 
 def generate_json_schema(json_data):
+    """_summary_
+
+    Args:
+        json_data (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     schema = {}
 
     def walk(current_data, current_schema):
@@ -26,6 +34,12 @@ def generate_json_schema(json_data):
 
 
 def generate_json_schema_v2(json_data):
+    """_summary_
+
+    Args:
+        json_data (_type_): _description_
+    """
+
     def parse_object(obj):
         schema = {"type": "object", "properties": {}, "required": []}
         for key, value in obj.items():
@@ -40,6 +54,29 @@ def generate_json_schema_v2(json_data):
             return {"type": "array", "items": parse(arr[0])}
 
     def parse(value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        type_mapping = {
+            dict: parse_object,
+            list: parse_array,
+            bool: "boolean",
+            int: "integer",
+            float: "number",
+            type(None): "null",
+        }
+        value_type = type(value)
+        if isinstance(value, dict) or isinstance(value, list):
+            return type_mapping[value_type](value)
+        else:
+            return {"type": type_mapping.get(value_type, "string")}
+
+    def parse_old(value):
         if isinstance(value, dict):
             return parse_object(value)
         elif isinstance(value, list):
